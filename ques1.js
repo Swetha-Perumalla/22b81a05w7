9d0e9adbc4d42732aba25732087e338e9c70f062
Swetha-Perumalla <ssperumalla7@gmail.com>
2025-02-04 15:22:17 +0530

    Modification commit

--- ques1.js
+++ ques1.js
@@ -1,26 +1,3 @@
-// function displayMemoryUsage() {
-//     setInterval(() => {
- 
-//         if (navigator.deviceMemory) {
-//             const totalMemory = navigator.deviceMemory;
-//             const freeMemory = totalMemory * 0.75; 
-//             const usedMemory = totalMemory - freeMemory;
-
-//             const freeMemoryPercentage = (freeMemory / totalMemory) * 100;
-
-//             console.clear();
-//             console.log(`Total Memory: ${totalMemory} GB`);
-//             console.log(`Used Memory: ${usedMemory.toFixed(2)} GB`);
-//             console.log(`Free Memory: ${freeMemory.toFixed(2)} GB`);
-//             console.log(`Percentage of Free Memory: ${freeMemoryPercentage.toFixed(2)}%`);
-//             console.log("-".repeat(40));
-//         } else {
-//             console.log("Device memory information is not available in your browser.");
-//         }
-//     }, 5000);
-// }
-
-// displayMemoryUsage();
 const os = require('os');
 
 function displayMemoryUsage() {
